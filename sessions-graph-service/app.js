fed10bcaf31b97ba83bb2d2e9e1a7b231c4e6641
Nag <nagcloudlab@gmail.com>
2024-09-12 16:43:54 +0530

    apollo server data sources

--- sessions-graph-service/app.js
+++ sessions-graph-service/app.js
@@ -1,13 +1,23 @@
 const { ApolloServer } = require('apollo-server');
 
+const SessionAPI = require('./datasources/sessions')
+const SpeakerAPI = require('./datasources/speakers')
+
+
 
 const typeDefs = require('./typedefs')
 const resolvers = require('./resolvers')
+
+const dataSources = () => ({
+    sessionAPI: new SessionAPI(),
+    speakerAPI: new SpeakerAPI()
+})
 
 
 const server = new ApolloServer({
     typeDefs,
     resolvers,
+    dataSources,
 });
 
 
